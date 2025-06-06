b1f2d6421a91899c951d648a84b05ee9511f5642
= <mohamedabobakr04@gmail.com>
2025-06-06 22:55:03 +0200

    added index file for middlewares

---
+++ src/middlewares/index.js
@@ -0,0 +1,31 @@
+import addressesValidation from "./addressesValidation";
+import { asyncWrapper } from "./asyncWrapper";
+import categoriesBrandsValidation from "./categoriesBrandsValidation";
+import discountsValidation from "./discountsValidation";
+import globalErrorHandler from "./globalErrorHandler";
+import imageValidation from "./imageValidation";
+import isAllowed from "./isAllowed";
+import loginValidation from "./loginValidation";
+import registerUserValidation from "./registerUserValidation";
+import verifyToken from "./verifyToken";
+import productValidation from "./productValidation";
+import validateRequestBody from "./validateRequestBody";
+import orderValidation from "./orderValidation";
+import reviewsValidation from "./reviewsValidation";
+
+export {
+  addressesValidation,
+  asyncWrapper,
+  categoriesBrandsValidation,
+  discountsValidation,
+  globalErrorHandler,
+  imageValidation,
+  isAllowed,
+  loginValidation,
+  registerUserValidation,
+  verifyToken,
+  productValidation,
+  validateRequestBody,
+  orderValidation,
+  reviewsValidation,
+};
