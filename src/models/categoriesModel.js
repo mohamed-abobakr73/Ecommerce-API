42c3ce401b4973e2c1c6a49d4e95a71dd5a7ecaa
= <mohamedabobakr04@gmail.com>
2025-06-06 14:34:02 +0200

    added categories model

---
+++ src/models/categoriesModel.js
@@ -0,0 +1,15 @@
+const createCategoriesModel = async () => {
+  try {
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS categories (
+        category_id INT PRIMARY KEY AUTO_INCREMENT,
+        category_name VARCHAR(50) NOT NULL
+      );
+    `);
+    console.log("✅ categories table ready");
+  } catch (error) {
+    console.error("❌ categories table error:", error);
+  }
+};
+
+export default createCategoriesModel;
---
+++ src/models/productsModel.js
@@ -0,0 +1,24 @@
+const createProductsTable = async () => {
+  try {
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS products (
+        product_id INT PRIMARY KEY AUTO_INCREMENT,
+        product_name VARCHAR(50) NOT NULL,
+        product_description TEXT NOT NULL,
+        price DECIMAL(6,2) NOT NULL,
+        stock_quantity INT NOT NULL,
+        seller_id INT NOT NULL,
+        category_id INT NOT NULL,
+        brand_id INT NOT NULL,
+        created_at DATE NOT NULL,
+        FOREIGN KEY (category_id) REFERENCES categories(category_id),
+        FOREIGN KEY (brand_id) REFERENCES brands(brand_id)
+      );
+    `);
+    console.log("✅ products table ready");
+  } catch (error) {
+    console.error("❌ products table error:", error);
+  }
+};
+
+export default createProductsTable;
