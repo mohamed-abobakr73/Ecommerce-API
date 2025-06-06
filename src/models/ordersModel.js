db99c718947f0000363d66fda8cd0e653e878718
= <mohamedabobakr04@gmail.com>
2025-06-06 14:39:15 +0200

    added orders model

---
+++ src/models/ordersModel.js
@@ -0,0 +1,20 @@
+const createOrdersTable = async () => {
+  try {
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS orders (
+        order_id INT PRIMARY KEY AUTO_INCREMENT,
+        user_id INT NOT NULL,
+        status ENUM('Pending', 'Shipped', 'Delivered', 'Canceled', 'Processing') NOT NULL,
+        total_price DECIMAL(6,2) NOT NULL,
+        discount_id INT,
+        created_at DATE NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        FOREIGN KEY (discount_id) REFERENCES discounts(discount_id)
+      );
+    `);
+  } catch (error) {
+    console.error("orders table error:", error);
+  }
+};
+
+export default createOrdersTable;
