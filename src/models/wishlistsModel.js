d6b2a14cb7e8de72b74954571c1cad59ef2bbb90
= <mohamedabobakr04@gmail.com>
2025-06-06 14:42:48 +0200

    added wishlists model

---
+++ src/models/wishlistsModel.js
@@ -0,0 +1,17 @@
+const createWishListsTable = async () => {
+  try {
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS wishlist (
+        wishlist_id INT PRIMARY KEY AUTO_INCREMENT,
+        user_id INT NOT NULL,
+        product_id INT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        FOREIGN KEY (product_id) REFERENCES products(product_id)
+      );
+    `);
+  } catch (error) {
+    console.error("wishlist table error:", error);
+  }
+};
+
+export default createWishListsTable;
