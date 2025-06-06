6099b41d270cf9584711179897025139ea4f8ea1
= <mohamedabobakr04@gmail.com>
2025-06-06 14:38:23 +0200

    added discounts model

---
+++ src/models/discountsModel.js
@@ -0,0 +1,18 @@
+const createDiscountsTable = async () => {
+  try {
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS discounts (
+        discount_id INT PRIMARY KEY AUTO_INCREMENT,
+        seller_id INT NOT NULL,
+        code VARCHAR(50) NOT NULL,
+        discount_percentage DECIMAL(4,2) NOT NULL,
+        valid_from DATETIME NOT NULL,
+        valid_to DATETIME NOT NULL
+      );
+    `);
+  } catch (error) {
+    console.error("discounts table error:", error);
+  }
+};
+
+export default createDiscountsTable;
