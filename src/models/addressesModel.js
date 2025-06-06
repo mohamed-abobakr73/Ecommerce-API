40b13db8afad303ce7c6bae5b5e57ec502d3b580
= <mohamedabobakr04@gmail.com>
2025-06-06 14:44:15 +0200

    added addresses table

---
+++ src/models/addressesModel.js
@@ -0,0 +1,21 @@
+const createAddressesTable = async () => {
+  try {
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS addresses (
+        address_id INT PRIMARY KEY AUTO_INCREMENT,
+        user_id INT NOT NULL,
+        address_line_1 VARCHAR(100) NOT NULL,
+        address_line_2 VARCHAR(100),
+        country VARCHAR(30) NOT NULL,
+        state VARCHAR(30) NOT NULL,
+        city VARCHAR(30) NOT NULL,
+        is_default BOOLEAN,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+      );
+    `);
+  } catch (error) {
+    console.error("addresses table error:", error);
+  }
+};
+
+export default createAddressesTable;
