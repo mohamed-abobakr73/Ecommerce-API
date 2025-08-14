d7712c4fb0516021342776b72af1b5492f7f4823
= <mohamedabobakr04@gmail.com>
2025-08-14 07:25:10 +0200

    fix: fixed start and dev commands paths to the root file

--- src/configs/cloudinaryConfig.js
+++ src/configs/cloudinaryConfig.js
@@ -1,11 +1,18 @@
 import cloudinary from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
+import { config } from "dotenv";
+
+config();
+
+const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 
 cloudinary.config({
-  cloud_name: "ddk228iop",
-  api_key: "395224322189611",
-  api_secret: "bwsCVTM6Zn4jS9PRZinWPXAJWCg",
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
